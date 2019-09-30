6d0e28ba21adff5e08227c83a8284535b9803ae2
Christian Pone <ChristiaNotarmaso@gmail.com>
2019-09-30 15:45:08 +0200

    Tilføjet Myndighedstjekker

---
+++ src/MyndighedsTjekker.java
@@ -0,0 +1,27 @@
+public class MyndighedsTjekker{
+
+    public static void main (String[] args){
+
+        int alder;
+
+        java.util.Scanner brugerinput = new java.util.Scanner(System.in);
+
+        alder = brugerinput.nextInt();
+
+        if (alder < 18) {
+
+            System.out.println("Du er ikke myndig!");
+
+        }
+
+        if (alder > 18) {
+
+            System.out.println("Du er myndig!");
+
+        }
+
+
+    }
+
+
+}
