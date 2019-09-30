54e1d251177eb2179867824b387a725a3432431b
Christian Pone <ChristiaNotarmaso@gmail.com>
2019-09-30 16:02:12 +0200

    Tilføjet Linjer

--- src/GitTestHer.java
+++ src/GitTestHer.java
@@ -2,9 +2,10 @@ public class GitTestHer {
 
     public static void main(String[] args) {
 
-        System.out.println("virk");
-
-        System.out.printf("new ");
+        System.out.println("Denne");
+        System.out.println("Test");
+        System.out.println("test");
+        System.out.println("Virker ");
 
     }
 
