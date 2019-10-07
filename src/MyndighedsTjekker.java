f8330858f8569efcaeb0e1ed18572f6875c92c56
Christian Pone <ChristiaNotarmaso@gmail.com>
2019-10-07 13:13:30 +0200

    Rettet fejl

--- src/MyndighedsTjekker.java
+++ src/MyndighedsTjekker.java
@@ -8,7 +8,7 @@ public class MyndighedsTjekker{
 
         alder = brugerinput.nextInt();
 
-        if (alder > 18) {
+        if (alder < 18) {
 
             System.out.println("Du er ikke myndig!");
 
