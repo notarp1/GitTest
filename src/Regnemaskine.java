242b1e0e082885a4ae7c6c637dfef255673f4d58
Amer Biro <amer.biro@gmail.com>
2019-10-07 13:22:52 +0200

    Regnemaskine tilføjet

---
+++ src/Regnemaskine.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Regnemaskine {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Indtast tal nummer 1");
+        int a = scan.nextInt();
+        System.out.println("Indtast tal nummer 2");
+        int b = scan.nextInt();
+        int resultat = a + b;
+        System.out.println(resultat);
+
+    }
+}
